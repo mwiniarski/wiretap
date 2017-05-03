eccf4b9e76dd353c38cc37586eead47037d03aff
RuRush47 <olekrurarz@gmail.com>
2017-05-03 20:35:58 +0200

    Add ScreenCapture class with capture screenshot function.

---
+++ Windows/WindowsStalker/WindowsStalker/ScreenCapture.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace WindowsStalker
+{
+    public class ScreenCapture
+    {
+        public static void CaptureScreenshot()
+        {
+            var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height,
+                PixelFormat.Format32bppArgb);
+
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                Screen.PrimaryScreen.Bounds.Y,
+                0,
+                0,
+                Screen.PrimaryScreen.Bounds.Size,
+                CopyPixelOperation.SourceCopy);
+
+            //TODO hash this
+            bmpScreenshot.Save("Screenshot.png", ImageFormat.Png);
+        }
+    }
+}
