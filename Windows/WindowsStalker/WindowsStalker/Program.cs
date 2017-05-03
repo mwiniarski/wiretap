2f044b7ffb6adbbc323e9a1d83769ed6ec5c852b
RuRush47 <olekrurarz@gmail.com>
2017-05-03 16:20:10 +0200

    Add initial console aplication

---
+++ Windows/WindowsStalker/WindowsStalker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WindowsStalker
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
