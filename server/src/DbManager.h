a1380aac5dc8877c2af8ee1d985e5d92c43760e4
mwiniarski <mw.pari@gmail.com>
2017-05-28 19:11:46 +0200

    Add files properly to database

--- server/src/DbManager.h
+++ server/src/DbManager.h
@@ -12,15 +12,18 @@ public:
     DbManager();
     ~DbManager(){delete connection;}
 
-    int isAccepted(int uuid, std::string s);
-    void addFile(int uuid, std::string s);
+    int isAccepted(std::string uuid);
+    std::string addFile(int id, std::string s, int file_type);
     bool connect();
     void disconnect();
 private:
     void readConfigs();
+    void throwError(std::string msg);
     std::string generateName(int uuid);
     pqxx::connection * connection;
     std::string configs[5];
+
+    std::string deviceType;
 };
 
 #endif
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -6,23 +6,13 @@
 #include <fstream>
 using namespace std;
 
-std::string getTimestamp()
-{
-    using namespace std::chrono;
-
-    int ret = duration_cast<milliseconds>
-    (system_clock::now().time_since_epoch()).count();
-
-    return std::to_string(ret);
-}
-
 int main(int argc, char** argv) {
 
     if(argc != 2) {
         cout << "Wrong arguments!" << endl;
         return 0;
     }
-    
+
     short port = atoi(argv[1]);
 
     Manager manager(port, port+1);
